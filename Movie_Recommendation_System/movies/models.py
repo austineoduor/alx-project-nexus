a16019f985f221e079edefaf1d8d3341637a48d8
austineoduor <austineoduor57@gmail.com>
2025-08-11 13:23:56 +0300

    projrct nexus

--- Movie_Recommendation_System/movies/models.py
+++ Movie_Recommendation_System/movies/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Movie(models.Model):
-    tmdb_id = models.IntegerField(unique=True, null=True )
-    title = models.CharField(max_length=255, null=True)
-    poster_url = models.URLField(blank=True, null=True)
+    tmdb_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    poster_url = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
